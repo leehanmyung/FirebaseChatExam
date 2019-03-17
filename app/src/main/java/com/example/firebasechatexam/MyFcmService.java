ec56b42bb37a03f3a823875aaa28e2aeab462c53
이한명 <hanmyung5725@gmail.com>
2019-03-17 11:56:41 +0900

    FCM 메시지 보내기

---
+++ app/src/main/java/com/example/firebasechatexam/MyFcmService.java
@@ -0,0 +1,19 @@
+package com.example.firebasechatexam;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFcmService extends FirebaseMessagingService {
+    public static final String TAG = MyFcmService.class.getSimpleName();
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        // 여기서 FCM 메시지를 처리함
+        Log.d(TAG, "onMessageReceived ID: " + remoteMessage.getMessageId());
+        Log.d(TAG, "onMessageReceived DATA: " + remoteMessage.getData());
+    }
+
+
+
+}
